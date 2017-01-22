8e8d9ab9722ec64aa49fbcfd68ba9b662f980001
tange3421 <b15.l.m.n.o@gmail.com>
2017-01-22 10:51:39 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -5,22 +5,27 @@ ta.Client.start(function(){
   $.getJSON('data.json'),function(){
     ta.Client.show(data);
     });
-});
 
-// subscribe to events
-taClient.subscribe('onError', function (error) {
-  console.log('TA Widget Sent Error: ' + error);
-});
+  // subscribe to events
+  taClient.subscribe('onError', function (error) {
+    console.log('TA Widget Sent Error: ' + error);
+  });
+
+  taClient.subscribe('doneClicked', function (op) {
+    console.log('final decision is ' + op.name);
+  });
+
+  taClient.subscribe('compareClicked', function(ops) {
+    console.log('comparing options:' + ops.map(function(op){return op.name;}));
+  });
+
+  taClient.subscribe('problemResolved', function(dillema) {
+    var opOps = dillema.resolution.solutions.filter(function(s){return s.status=='FRONT';});
+    console.log('Problem Resolved. '+ opOps.length + ' Top options.' );
+  });
+
+  var clk = taClient.subscribe('optionClicked', function(op) {
+    console.log('Clicked. '+ op.name);
+  });
   
-taClient.subscribe('doneClicked', function (op) {
-  console.log('final decision is ' + op.name);
-});
-
-taClient.subscribe('compareClicked', function(ops) {
-  console.log('comparing options:' + ops.map(function(op){return op.name;}));
-});
-
-taClient.subscribe('problemResolved', function(dillema) {
-  var opOps = dillema.resolution.solutions.filter(function(s){return s.status=='FRONT';});
-  console.log('Problem Resolved. '+ opOps.length + ' Top options.' );
 });
