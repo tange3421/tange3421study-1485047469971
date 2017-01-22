c8ebb391ce1c526953f5391172280060bda30ad6
tange3421 <b15.l.m.n.o@gmail.com>
2017-01-22 10:48:54 +0900

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,26 @@
+var taClient = new TradeoffAnalytics('ta');
+
+ta.Client.start(function(){
+//jsonファイルの取得
+  $.getJSON('data.json'),function(){
+    ta.Client.show(data);
+    });
+});
+
+// subscribe to events
+taClient.subscribe('onError', function (error) {
+  console.log('TA Widget Sent Error: ' + error);
+});
+  
+taClient.subscribe('doneClicked', function (op) {
+  console.log('final decision is ' + op.name);
+});
+
+taClient.subscribe('compareClicked', function(ops) {
+  console.log('comparing options:' + ops.map(function(op){return op.name;}));
+});
+
+taClient.subscribe('problemResolved', function(dillema) {
+  var opOps = dillema.resolution.solutions.filter(function(s){return s.status=='FRONT';});
+  console.log('Problem Resolved. '+ opOps.length + ' Top options.' );
+});
