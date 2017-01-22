a583c02846ba885174fab46c0265899ba7397641
tange3421 <b15.l.m.n.o@gmail.com>
2017-01-22 13:41:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-/*eslint-env node*/
+/**
+ * Copyright 2014-2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+'use strict';
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+var express = require('express'),
+    tradeoffAnalyticsConfig = require('./tradeoff-analytics-config');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
 var app = express();
+app.use('/',express.static(__dirname + '/public'));
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+// For local development, copy your service instance credentials here, otherwise you may ommit this parameter
+var serviceCredentials = {
+  username: '<username>',
+  password: '<password>'
+}
+// When running on Bluemix, serviceCredentials will be overriden by the credentials obtained from VCAP_SERVICES
+tradeoffAnalyticsConfig.setupToken(app, serviceCredentials); 
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// to communicate with the service using a proxy rather then a token, add a dependency on "body-parser": "^1.15.0" 
+// to package.json, and use:
+// tradeoffAnalyticsConfig.setupProxy(app, serviceCredentials);
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+var port = process.env.VCAP_APP_PORT || 2000;
+app.listen(port);
+console.log('listening at:', port);
